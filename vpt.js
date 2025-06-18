c85fdd8ef737a932ff7328874e7fca0a2b0172a6
Tilen Smrdel <69430904+TilenS2002@users.noreply.github.com>
2025-06-18 12:09:08 +0200

    added example code
    added example code to vpt.js and compute.wgsl for reference and help with coding

--- vpt.js
+++ vpt.js
@@ -155,3 +155,82 @@ fs.writeFile('image.ascii.pgm', data, (err) => {
     if (err) throw err;
 })
 
+// iz compute exampla od tle naprej
+/*
+async function main() {
+    const adapter = await navigator.gpu?.requestAdapter();
+    const device = await adapter?.requestDevice();
+
+    if (!device) {
+        fail('browser does not support WebGPU');
+        return;
+    }
+
+    const computeCode = await fetch('compute.wgsl').then(request => request.text());
+
+    const module = device.createShaderModule({
+        label: 'moj compute shader',
+        code: computeCode
+    });
+
+    const pipeline = device.createComputePipeline({
+        label: 'moj compute pipeline',
+        layout: 'auto',
+        compute: {
+            module
+        }
+    });
+
+    const input = new Float32Array([1,3,5]);
+
+    const workBuffer = device.createBuffer({
+        label: 'moj work buffer',
+        size: input.byteLength,
+        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST
+    });
+
+    device.queue.writeBuffer(workBuffer, 0, input);
+
+    const resultBuffer = device.createBuffer({
+        label: 'moj buffer za rezultat',
+        size: input.byteLength,
+        usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST
+    });
+
+    const bindGroup = device.createBindGroup({
+        label: 'bindGroup za work buffer',
+        layout: pipeline.getBindGroupLayout(0),
+        entries: [
+            { binding: 0, resource: { buffer: workBuffer } },
+        ],
+    });
+
+    const encoder = device.createCommandEncoder({
+        label: 'encoder'
+    });
+
+    const pass = encoder.beginComputePass({
+        label: 'pass za encoder'
+    });
+
+    pass.setPipeline(pipeline);
+    pass.setBindGroup(0, bindGroup);
+    pass.dispatchWorkgroups(input.length);
+    pass.end();
+
+    encoder.copyBufferToBuffer(workBuffer, 0, resultBuffer, 0, resultBuffer.size);
+
+    const commandBuffer = encoder.finish();
+
+    device.queue.submit([commandBuffer]);
+
+    await resultBuffer.mapAsync(GPUMapMode.READ);
+    const result = new Float32Array(resultBuffer.getMappedRange());
+
+    console.log('input', input);
+    console.log('output', result);
+}
+
+main();
+*/
+
